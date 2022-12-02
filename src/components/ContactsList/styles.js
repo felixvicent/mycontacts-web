7db1c7f9bc694bc2dcffb3ee09347da428cffd2d
Félix Vicente <felixvicent1306@gmail.com>
2022-12-02 13:40:39 -0300

    Create contacts list

--- src/components/ContactsList/styles.js
+++ src/components/ContactsList/styles.js
@@ -11,7 +11,6 @@ export const Header = styled.div`
 
   strong {
     font-size: 22px;
-    color: #222;
   }
 
   a {
@@ -26,6 +25,75 @@ export const Header = styled.div`
     &:hover {
       background: ${({ theme }) => theme.colors.primary.main};
       color: #fff;
+    }
+  }
+`;
+
+export const ListContainer = styled.div`
+  margin-top: 24px;
+
+  header {
+    margin-bottom: 8px;
+
+    button {
+      background-color: transparent;
+      border: none;
+      display: flex;
+      align-items: center;
+
+      span {
+        color: ${({ theme }) => theme.colors.primary.main};
+        margin-right: 8px;
+        font-weight: bold;
+      }
+    }
+  }
+`;
+
+export const Card = styled.div`
+  background-color: #fff;
+  padding: 16px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  border-radius: 4px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  & + & {
+    margin-top: 16px;
+  }
+
+  .info {
+    .contact-name {
+      display: flex;
+      align-items: center;
+
+      small {
+        background-color: ${({ theme }) => theme.colors.primary.lighter};
+        color: ${({ theme }) => theme.colors.primary.main};
+        font-weight: bold;
+        text-transform: uppercase;
+        padding: 4px;
+        border-radius: 4px;
+        margin-left: 8px;
+      }
+    }
+
+    span {
+      display: block;
+      font-size: 14px;
+      color: ${({ theme }) => theme.colors.gray[200]};
+    }
+  }
+
+  .actions {
+    display: flex;
+    align-items: center;
+
+    button {
+      background-color: transparent;
+      border: none;
+      margin-left: 8px;
     }
   }
 `;
