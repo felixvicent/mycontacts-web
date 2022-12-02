ebe09a2f5c946294d9cbd9f55c4cf320dfe1ace9
Félix Vicente <felixvicent1306@gmail.com>
2022-12-02 18:36:47 -0300

    Create Modal component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export default styled.button`
-  width: 100%;
   height: 52px;
+  padding: 0 16px;
   background-color: ${({ theme }) => theme.colors.primary.main};
   border: none;
   border-radius: 4px;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
+import { Modal } from '../../components/Modal';
 
 import {
   Container,
@@ -15,6 +16,7 @@ import {
 export function Home() {
   return (
     <Container>
+      <Modal />
       <InputSearchContainer>
         <input type="text" name="" id="" placeholder="Pesquisar contato" />
       </InputSearchContainer>
