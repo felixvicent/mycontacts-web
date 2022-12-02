4c9df179a1c85f3f890cb4e4eac14be3d839351e
Félix Vicente <felixvicent1306@gmail.com>
2022-12-02 19:23:05 -0300

    Create loader component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
-import { Modal } from '../../components/Modal';
+import { Loader } from '../../components/Loader';
 
 import {
   Container,
@@ -16,7 +16,7 @@ import {
 export function Home() {
   return (
     <Container>
-      <Modal danger />
+      <Loader />
       <InputSearchContainer>
         <input type="text" name="" id="" placeholder="Pesquisar contato" />
       </InputSearchContainer>
