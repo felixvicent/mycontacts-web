4ed75b6c75244626c35a31c68815415324ef876f
Félix Vicente <felixvicent1306@gmail.com>
2022-12-02 14:17:35 -0300

    Refact header & home page

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,11 @@
 import logo from '../../assets/images/logo.svg';
 
-import { Container, InputSearchContainer } from './styles';
+import { Container } from './styles';
 
 export function Header() {
   return (
     <Container>
       <img src={logo} alt="MyContacts" width="201" />
-
-      <InputSearchContainer>
-        <input type="text" name="" id="" placeholder="Pesquisar contato" />
-      </InputSearchContainer>
     </Container>
   );
 }
