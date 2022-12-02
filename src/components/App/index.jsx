1e8c0cd5068292c322e9c5d3c59cdee2b415efaa
Félix Vicente <felixvicent1306@gmail.com>
2022-12-02 14:05:33 -0300

    Configure routes

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,22 +1,25 @@
 import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
 
 import GlobalStyles from '../../assets/styles/global';
 import defaltTheme from '../../assets/styles/themes/default';
 
 import { Header } from '../Header';
-import { ContacsList } from '../ContactsList';
 
 import { Container } from './styles';
+import { Routes } from '../../routes';
 
 function App() {
   return (
-    <ThemeProvider theme={defaltTheme}>
-      <GlobalStyles />
-      <Container>
-        <Header />
-        <ContacsList />
-      </Container>
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={defaltTheme}>
+        <GlobalStyles />
+        <Container>
+          <Header />
+          <Routes />
+        </Container>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
