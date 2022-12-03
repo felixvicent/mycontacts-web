7e85e3cfd512d2637e9ac2895d328cb79aee30d1
Félix Vicente <felixvicent1306@gmail.com>
2022-12-03 13:49:27 -0300

    Validation name input

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -1,18 +1,34 @@
-import { useState } from 'react';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
+import Button from '../Button';
 import { FormGroup } from '../FormGroup';
 import Input from '../Input';
 import Select from '../Select';
-import Button from '../Button';
 
-import { Form, ButtonContainer } from './styles';
+import { ButtonContainer, Form } from './styles';
 
 export function ContactForm({ buttonLabel }) {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
   const [category, setCategory] = useState('');
+  const [errors, setErrors] = useState([]);
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+
+    if (!event.target.value) {
+      setErrors((prevState) => [
+        ...prevState,
+        { field: 'name', message: 'Nome é obrigatório' },
+      ]);
+    } else {
+      setErrors((prevState) =>
+        prevState.filter((error) => error.field !== 'name'),
+      );
+    }
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -21,11 +37,7 @@ export function ContactForm({ buttonLabel }) {
   return (
     <Form onSubmit={handleSubmit}>
       <FormGroup>
-        <Input
-          placeholder="Nome"
-          value={name}
-          onChange={(event) => setName(event.target.value)}
-        />
+        <Input placeholder="Nome" value={name} onChange={handleNameChange} />
       </FormGroup>
       <FormGroup>
         <Input
