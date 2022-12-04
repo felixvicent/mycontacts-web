60a9b20b74eae9d3256119fbd04487a06a1540ba
Félix Vicente <felixvicent1306@gmail.com>
2022-12-04 16:25:49 -0300

    Filtering contacts list

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -18,6 +18,11 @@ import {
 export function Home() {
   const [contacts, setContacts] = useState([]);
   const [orderBy, setOrderBy] = useState('asc');
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(searchTerm.toLowerCase()),
+  );
 
   useEffect(() => {
     fetch(`http://localhost:3333/contacts?orderBy=${orderBy}`).then(
@@ -32,27 +37,39 @@ export function Home() {
     setOrderBy((prevState) => (prevState === 'asc' ? 'desc' : 'asc'));
   }
 
+  function handleChangeSearchTerm(event) {
+    setSearchTerm(event.target.value);
+  }
+
   return (
     <Container>
       <InputSearchContainer>
-        <input type="text" name="" id="" placeholder="Pesquisar contato" />
+        <input
+          value={searchTerm}
+          onChange={handleChangeSearchTerm}
+          type="text"
+          placeholder="Pesquisar contato"
+        />
       </InputSearchContainer>
 
       <Header>
         <strong>
-          {contacts.length} {contacts.length === 1 ? 'Contato' : 'Contatos'}
+          {filteredContacts.length}{' '}
+          {filteredContacts.length === 1 ? 'Contato' : 'Contatos'}
         </strong>
         <Link to="/new">Novo contato</Link>
       </Header>
 
-      <ListHeader orderBy={orderBy}>
-        <button onClick={handleToggleOrderBy} type="button">
-          <span>Nome</span>
-          <img src={arrow} alt="Seta" />
-        </button>
-      </ListHeader>
+      {filteredContacts.length > 0 && (
+        <ListHeader orderBy={orderBy}>
+          <button onClick={handleToggleOrderBy} type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="Seta" />
+          </button>
+        </ListHeader>
+      )}
 
-      {contacts.map((contact) => (
+      {filteredContacts.map((contact) => (
         <Card key={contact.id}>
           <div className="info">
             <div className="contact-name">
