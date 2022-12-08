b733b18f6a7ac77e6ccc8e9e5fcb9add2bddbd9f
Félix Vicente <felixvicent1306@gmail.com>
2022-12-08 08:08:06 -0300

    Show empty search list  message

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -12,6 +12,7 @@ import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
 import sad from '../../assets/images/sad.svg';
 import emptyBox from '../../assets/images/empty-box.svg';
+import magnifierQuestion from '../../assets/images/magnifier-question.svg';
 
 import { Loader } from '../../components/Loader';
 import Button from '../../components/Button';
@@ -24,6 +25,7 @@ import {
   Card,
   ErrorContainer,
   EmptyListContainer,
+  SearchNotFoundContainer,
 } from './styles';
 
 export function Home() {
@@ -137,6 +139,17 @@ export function Home() {
                 <img src={arrow} alt="Seta" />
               </button>
             </ListHeader>
+          )}
+
+          {contacts.length > 0 && filteredContacts.length < 1 && (
+            <SearchNotFoundContainer>
+              <img src={magnifierQuestion} alt="Magnifier Question" />
+
+              <span>
+                Nenhum resultado foi encontrado para{' '}
+                <strong>”{searchTerm}”</strong>.
+              </span>
+            </SearchNotFoundContainer>
           )}
 
           {filteredContacts.map((contact) => (
