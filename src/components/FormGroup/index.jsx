7afedb8f67482cc7ff672cb9ff7c5ddca215bc9b
Félix Vicente <felixvicent1306@gmail.com>
2022-12-08 09:05:44 -0300

    Create spinner component

--- src/components/FormGroup/index.jsx
+++ src/components/FormGroup/index.jsx
@@ -1,12 +1,19 @@
 import PropTypes from 'prop-types';
 
+import { Spinner } from '../Spinner';
+
 import { Container } from './styles';
 
 export function FormGroup({ children, error, isLoading }) {
   return (
     <Container>
       <div className="form-item">
-        {children} {isLoading && <div className="loader" />}
+        {children}{' '}
+        {isLoading && (
+          <div className="loader">
+            <Spinner size={16} />
+          </div>
+        )}
       </div>
       {error && <small>{error}</small>}
     </Container>
--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 
 import { Overlay } from './styles';
 
+import { Spinner } from '../Spinner';
+
 export function Loader({ isLoading }) {
   if (!isLoading) {
     return null;
@@ -10,7 +12,7 @@ export function Loader({ isLoading }) {
 
   return ReactDOM.createPortal(
     <Overlay>
-      <div className="loader" />
+      <Spinner size={90} />
     </Overlay>,
     document.getElementById('loader-root'),
   );
