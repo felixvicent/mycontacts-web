fea1b6332f41c513cb66331113b976a41b15f7da
Félix Vicente <felixvicent1306@gmail.com>
2022-12-08 16:21:38 -0300

    Get data for edit contact

--- src/pages/EditContact/index.jsx
+++ src/pages/EditContact/index.jsx
@@ -1,11 +1,43 @@
+import { useParams, useHistory } from 'react-router-dom';
+
+import { useEffect, useState } from 'react';
 import { PageHeader } from '../../components/PageHeader';
 import { ContactForm } from '../../components/ContactForm';
+import { Loader } from '../../components/Loader';
+
+import toast from '../../utils/toast';
+
+import ContactsService from '../../services/ContactsService';
 
 export function EditContact() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const history = useHistory();
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    async function loadContact() {
+      try {
+        const contactData = await ContactsService.getContactById(id);
+
+        console.log(contactData);
+        setIsLoading(false);
+      } catch {
+        history.push('/');
+        toast({ type: 'danger', text: 'Contato não encontrado!' });
+      }
+    }
+
+    loadContact();
+  }, [id, history]);
+
+  function handleSubmit() {}
   return (
     <>
+      <Loader isLoading={isLoading} />
       <PageHeader title="Editar Félix Vicente" />
-      <ContactForm buttonLabel="Salvar Alterações" />
+      <ContactForm onSubmit={handleSubmit} buttonLabel="Salvar Alterações" />
     </>
   );
 }
