46509a64426aa30a7e9c1d19caba3a7287e5dbc0
Félix Vicente <felixvicent1306@gmail.com>
2022-12-08 16:07:50 -0300

    Clear form after submit

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -75,6 +75,10 @@ export function ContactForm({ buttonLabel, onSubmit }) {
     await onSubmit({ name, email, phone, categoryId });
 
     setIsSubmitting(false);
+    setName('');
+    setEmail('');
+    setPhone('');
+    setCategoryId('');
   }
 
   return (
