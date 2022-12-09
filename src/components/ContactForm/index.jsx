0d84e95db3fcf525eeab7d1de4ab70f3ff345581
Félix Vicente <felixvicent1306@gmail.com>
2022-12-09 16:53:49 -0300

    Reset form on create contact

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -33,6 +33,12 @@ export const ContactForm = forwardRef(({ buttonLabel, onSubmit }, ref) => {
         setPhone(formatPhone(contact.phone ?? ''));
         setCategoryId(contact.category_id ?? '');
       },
+      resetFields: () => {
+        setName('');
+        setEmail('');
+        setPhone('');
+        setCategoryId('');
+      },
     }),
     [],
   );
--- src/pages/NewContact/index.jsx
+++ src/pages/NewContact/index.jsx
@@ -1,9 +1,13 @@
+import { useRef } from 'react';
+
 import { ContactForm } from '../../components/ContactForm';
 import { PageHeader } from '../../components/PageHeader';
 import ContactsService from '../../services/ContactsService';
 import toast from '../../utils/toast';
 
 export function NewContact() {
+  const contactFormRef = useRef(null);
+
   async function handleSubmit(formData) {
     try {
       const contact = {
@@ -14,6 +18,8 @@ export function NewContact() {
       };
       await ContactsService.createContact(contact);
 
+      contactFormRef.current.resetFields();
+
       toast({ type: 'success', text: 'Contato cadastrado com sucesso!' });
     } catch {
       toast({ type: 'danger', text: 'Ocorreu um erro ao cadastrar contato!' });
@@ -23,7 +29,11 @@ export function NewContact() {
   return (
     <>
       <PageHeader title="Novo Contato" />
-      <ContactForm buttonLabel="Cadastrar" onSubmit={handleSubmit} />
+      <ContactForm
+        ref={contactFormRef}
+        buttonLabel="Cadastrar"
+        onSubmit={handleSubmit}
+      />
     </>
   );
 }
