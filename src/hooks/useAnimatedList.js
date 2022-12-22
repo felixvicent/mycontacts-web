217aa867ea3347444c168458450516b9e9141b8c
Félix Vicente <felixvicent1306@gmail.com>
2022-12-22 12:41:05 -0300

    Add listeners to animationend

--- src/hooks/useAnimatedList.js
+++ src/hooks/useAnimatedList.js
@@ -1,22 +1,38 @@
-import { useCallback, useRef, useState, createRef } from 'react';
+import { useCallback, useRef, useState, createRef, useEffect } from 'react';
 
 export default function useAnimatedList(initialValue = []) {
   const [items, setItems] = useState(initialValue);
   const [pendingRemovalItemsId, setPendingRemovalItemsId] = useState([]);
 
   const animatedRefs = useRef(new Map());
+  const animationEndListeners = useRef(new Map());
+
+  const handleAnimationEnd = useCallback((id) => {
+    setItems((prevState) => prevState.filter((item) => item.id !== id));
+
+    setPendingRemovalItemsId((prevState) =>
+      prevState.filter((itemId) => itemId !== id),
+    );
+  }, []);
+
+  useEffect(() => {
+    pendingRemovalItemsId.forEach((itemId) => {
+      const animatedRef = animatedRefs.current.get(itemId);
+      const alreadyHasListener = animationEndListeners.current.has(itemId);
+
+      if (animatedRef?.current && !alreadyHasListener) {
+        animationEndListeners.current.set(itemId, true);
+
+        animatedRef.current.addEventListener('animationend', () => {
+          handleAnimationEnd(itemId);
+        });
+      }
+    });
+  }, [pendingRemovalItemsId, handleAnimationEnd]);
 
   const handleRemoveItem = useCallback((id) => {
     setPendingRemovalItemsId((prevState) => [...prevState, id]);
   }, []);
-
-  // const handleAnimationEnd = useCallback((id) => {
-  //   setItems((prevState) => prevState.filter((item) => item.id !== id));
-
-  //   setPendingRemovalItemsId((prevState) =>
-  //     prevState.filter((itemId) => itemId !== id),
-  //   );
-  // }, []);
 
   const getAnimatedRef = useCallback((itemId) => {
     let animatedRef = animatedRefs.current.get(itemId);
@@ -33,7 +49,6 @@ export default function useAnimatedList(initialValue = []) {
     (renderItem) =>
       items.map((item) => {
         const isLeaving = pendingRemovalItemsId.includes(item.id);
-
         const animatedRef = getAnimatedRef(item.id);
 
         return renderItem(item, { isLeaving, animatedRef });
